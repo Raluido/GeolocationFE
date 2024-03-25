e4a8ff6b71ed83de64903ea21fa7b89256e47b6b
“raluido” <raluido@gmail.com>
2024-03-25 21:16:13 +0000

    angular.json conf and Environment conf

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://capturaforest.agrestaweb.org/backend/public/api/reports-test',
-    myApiUrl: 'http://localhost:8080/api',
+    myApiUrl: 'http://raluido.ddns.net:8008/api',
     apiUrlGeol: 'https://api.geoapify.com/v1/geocode/search?text=',
     apiKey: 'b1bedbc082fc422d80eb73a3fc93259d',
 };
